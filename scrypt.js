997329c408aef16b2c6b6293b74b48e889b2a149
christalinabrx <bruxariadigital@gmail.com>
2024-03-05 01:07:20 -0300

    arrumandinho

--- scrypt.js
+++ scrypt.js
@@ -12,4 +12,39 @@ video.addEventListener("click", function () {
             video.webkitRequestFullscreen();
         }
     }
+
+    const video = document.getElementById("myVideo");
+
+// Defina o tamanho inicial do player
+let playerSize = 100;
+
+video.addEventListener("click", function () {
+    if (video.paused) {
+        video.play();
+        zoomIn();
+    } else {
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+        } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen();
+        }
+    }
+});
+
+function zoomIn() {
+    // Aumente gradualmente o tamanho do player a cada 100ms
+    const zoomInterval = setInterval(function () {
+        playerSize += 1;
+
+        // Limite o tamanho do player a 170%
+        if (playerSize <= 170) {
+            video.style.width = `${playerSize}%`;
+        } else {
+            clearInterval(zoomInterval);
+        }
+    }, 100);
+}
+
 });
