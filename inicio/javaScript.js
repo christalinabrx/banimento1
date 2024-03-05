3796d39c5bbb61c846dd3957d9df1fa6e07fe3a0
christalinabrx <bruxariadigital@gmail.com>
2024-03-05 10:52:52 -0300

    SIMPLIFICANDO

--- inicio/javaScript.js
+++ inicio/javaScript.js
@@ -1,13 +1,5 @@
-const startButton = document.getElementById("startButton");
 
-startButton.addEventListener("click", function () {
-    playMouseClickSound();
-    setTimeout(function(){
-        window.location.href = "https://christalinabrx.github.io/banimento1/";
-    }, 300); // Redirecionar após 300ms (ajuste conforme necessário)
-});
 
-function playMouseClickSound() {
-    const audio = new Audio("https://toque123.com/wp-content/uploads/2021/11/Windows-XP-Startup.mp3");
-    audio.play();
+function startBanimento() {
+    window.location.href = "https://christalinabrx.github.io/banimento1/";
 }
