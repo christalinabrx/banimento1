95e33276da75ff8c84a5d860da8f8d180f3cd307
christalinabrx <140367125+christalinabrx@users.noreply.github.com>
2024-03-05 09:19:07 -0300

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,20 +1,3 @@
-const video = document.getElementById("banimento1");
-
-video.addEventListener("click", function () {
-    if (video.paused) {
-        video.play();
-    } else {
-        if (video.requestFullscreen) {
-            video.requestFullscreen();
-        } else if (video.mozRequestFullScreen) {
-            video.mozRequestFullScreen();
-        } else if (video.webkitRequestFullscreen) {
-            video.webkitRequestFullscreen();
-        }
-    }
-
-    const video = document.getElementById("banimento1");
-
 // Defina o tamanho inicial do player
 let playerSize = 100;
 
